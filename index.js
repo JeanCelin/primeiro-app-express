9567b6d26fad772ebbf81d2809af28b73a956002
Jean Celin <jean_estudos@hotmail.com>
2025-11-11 17:46:18 -0300

    add route get by id

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from "express";
 
 const server = express();
 
-server.get("/books", (req, res) => {
-  return res.json([
+const data = [
     {
       id: 1,
       title: "Star Wars",
@@ -154,7 +153,17 @@ server.get("/books", (req, res) => {
       anoPublicacao: 1997,
       paginas: 336,
     },
-  ]);
+  ]
+
+server.get("/books", (req, res) => {
+  return res.json(data);
 });
+
+server.get("/books/:id", (req, res)=>{
+  const { id } = req.params
+
+ const book = data.find(book => book.id === Number(id))  
+ return res.json(book)
+})
 
 server.listen(3000);
